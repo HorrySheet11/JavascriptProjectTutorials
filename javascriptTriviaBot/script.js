9683cfc008d4216a6c4e8756d25791873a138ee5
Nat Marn <112994197+HorrySheet11@users.noreply.github.com>
2025-08-27 20:22:48 +0800

    Add files via upload

---
+++ javascriptTriviaBot/script.js
@@ -0,0 +1,23 @@
+
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName="Horry Sheet";
+let botLocation="Earth";
+let favoriteLanguage="JS";
+
+console.log("My name is "+botName+" and I live on "+botLocation+'.');
+console.log("My favorite programming language is "+favoriteLanguage+'.');
+
+let codingFact="Did you know that "+favoriteLanguage+" is a language and is my favorite";
+
+console.log(codingFact);
+
+codingFact= favoriteLanguage + " is like Java but is not.";
+
+console.log(codingFact);
+
+codingFact="This is my last message about "+favoriteLanguage+". ";
+
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - "+botName+" from "+botLocation+'.')
